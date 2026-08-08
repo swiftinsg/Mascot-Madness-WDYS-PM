bdf58ad4c0b6efa55ac2efff0570ed36bb79b591
Ekansh Mishra <158389986+Ekansh-Mishra1@users.noreply.github.com>
2026-08-08 15:21:28 +0800

    right arm

--- MascotMadness/Arms/RightArmView.swift
+++ MascotMadness/Arms/RightArmView.swift
@@ -3,18 +3,48 @@ import SwiftUI
 var rightArm: some BodyPart {
     RightArm {
         RightUpperArmSegment {
-            ZStack {
+            ZStack{
                 Rectangle()
-                    .frame(width: 30, height: 200)
-                    .foregroundStyle(.orange)
+                    .frame(width: 40, height: 200)
+                    .foregroundStyle(.red)
+                VStack(){
+                    Image(systemName: "moon.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                }
             }
         }
     } foreArm: {
         RightLowerArmSegment {
-            ZStack {
-                Rectangle()
-                    .frame(width: 30, height: 200)
-                    .foregroundStyle(.red)
+            ZStack{
+                VStack(){
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 30, height: 200)
+                            .foregroundStyle(Color(red: 227/255, green: 226/255, blue: 223/255))
+                        VStack{
+                            Text("N")
+                            Text("D")
+                            Text("P")
+                            Text("2")
+                            Text("0")
+                            Text("2")
+                            Text("6")
+                        }
+                    }
+                    Text("✋")
+                        .font(.largeTitle)
+                        .rotationEffect(.degrees(180))
+                }
             }
         }
     }
